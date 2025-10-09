fa80435e656cdf48efed3f791b707bd634715254
Patrick Stephens <pat@fluent.do>
2025-10-09 10:20:05 +0100

    refactor: remove custom directory
    Signed-off-by: Patrick Stephens <pat@fluent.do>

--- custom/cmake/enterprise.cmake
+++
@@ -1,7 +0,0 @@
-message(STATUS "Running Enterprise build set up")
-FLB_DEFINITION(FLB_ENTERPRISE)
-
-# For legacy builds we need to handle this explicitly in case it is removed from the source
-if(CMAKE_INSTALL_PREFIX MATCHES "/opt/td-agent-bit")
-  set(FLB_TD ON)
-endif()
