629fd8ca4d50b32ed89b7e8078aa399c415c4af0
Patrick Stephens <pat@fluent.do>
2025-11-07 11:37:20 +0000

    fix: add missing SASL development libraries for IAM support at runtime
    Signed-off-by: Patrick Stephens <pat@fluent.do>

--- source/cmake/enterprise.cmake
+++ source/cmake/enterprise.cmake
@@ -5,3 +5,16 @@ FLB_DEFINITION(FLB_ENTERPRISE)
 if(CMAKE_INSTALL_PREFIX MATCHES "/opt/td-agent-bit")
   set(FLB_TD ON)
 endif()
+
+# Ensure we have specific options enabled (they may get disabled implicitly due to missing dependencies)
+function(validate_required_options)
+    set(REQUIRED_OPTIONS ${ARGV})
+
+    foreach(OPT ${REQUIRED_OPTIONS})
+        if(NOT ${OPT})
+            message(FATAL_ERROR "ERROR: ${OPT} is required but disabled.")
+        endif()
+    endforeach()
+
+    message(STATUS "All required options validated successfully")
+endfunction()
